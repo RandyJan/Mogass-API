98e3f6b5950bec9a74040b444b67358987893e0f
Randy Jan <randyjan250@gmail.com>
2023-10-19 15:09:25 -0700

    added syncTable

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\authController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\authController;
+use App\Http\Controllers\syncTableController;
 use App\Http\Controllers\syncTransController;
 use App\Http\Controllers\syncEarningsController;
-use App\Http\Controllers\syncPointsinquiryController;
 use App\Http\Controllers\syncRedemptionController;
+use App\Http\Controllers\syncPointsinquiryController;
 
 
 /*
@@ -27,6 +28,7 @@ Route::group(['middleware'=> ['auth:sanctum']], function () {
 
 Route::post('/logout',[authcontroller::class, 'logout']);
 Route::get('/syncPointsinquiry/{id}',[syncPointsinquiryController::class, 'show']);
+Route::post('/syncTable/{table}/{branchid}',[syncTableController::class, 'show']);
 Route::post('/syncEarnings',[syncEarningsController::class, 'store']);
 Route::post('/syncTrans',[syncTransController::class, 'store']);
 Route::post('/syncRedemption', [syncRedemptionController::class, 'store']);
